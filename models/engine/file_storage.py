e6d27f8eec69d2b093795c26f74c5e3757ac9522
Mercileen <kamwindamercy@gmail.com>
2024-02-18 08:14:58 -0800

    Store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,73 +1,75 @@
 #!/usr/bin/python3
-"""
-Module: file_storage.py
 
-Defines a `FileStorage` class.
+"""File Storage class
+for serialization into a JSON file and
+deserialization of JSON file
+into an instances.
 """
-import os
+
 import json
+from os import path
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.review import Review
 from models.amenity import Amenity
 from models.place import Place
+from models.review import Review
 
 
-class FileStorage():
+class FileStorage:
+
     """
-    serializes instances to a JSON file and
-    deserializes JSON file to instances
+        Private class attributes:
+    __file_path: string - path to the JSON file
+    __objects: dictionary - empty but will store all objects by <class name>.id
     """
 
-    __file_path = "file.json"
-    __objects = {}
+    CLASSES = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review
+    }
+
+    __file_path = "file.json"  # path to the JSON file
+    __objects = {}  # dictionary to store all objects by <class name>.id
 
     def all(self):
-        """
-        returns the dictionary __objects
-        """
-        return FileStorage.__objects
+        """Returns the dictionary __objects."""
+        return self.__objects
 
     def new(self, obj):
-        """
-        sets in __objects the obj with key <obj class name>.id
-        """
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """
-        serializes __objects to the JSON file (path: __file_path)
-        """
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(
-                {k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
         """
-        deserializes the JSON file to __objects only if the JSON
-        file exists; otherwise, does nothing
+        Deserializes the JSON file to __objects.
+        Only if the JSON file (__file_path) exists; otherwise, do nothing.
+        If the file does not exist, no exception should be raised.
         """
-        current_classes = {'BaseModel': BaseModel, 'User': User,
-                           'Amenity': Amenity, 'City': City, 'State': State,
-                           'Place': Place, 'Review': Review}
-
-        if not os.path.exists(FileStorage.__file_path):
-            return
-
-        with open(FileStorage.__file_path, 'r') as f:
-            deserialized = None
-
-            try:
-                deserialized = json.load(f)
-            except json.JSONDecodeError:
-                pass
-
-            if deserialized is None:
-                return
-
-            FileStorage.__objects = {
-                k: current_classes[k.split('.')[0]](**v)
-                for k, v in deserialized.items()}
+        if path.exists(self.__file_path):
+            with open(self.__file_path, 'r', encoding="utf-8") as file:
+                serialized_objects = json.load(file)
+                for key, obj_data in serialized_objects.items():
+                    class_name, obj_id = key.split('.')
+                    # Dynamically create an instance of
+                    # the class based on class_name
+                    obj_class = globals()[class_name]
+                    obj_instance = obj_class(**obj_data)
+                    # Store the instance in __objects
+                    self.__objects[key] = obj_instance
